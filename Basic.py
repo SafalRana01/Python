6b8b6b68f5f77321b324cdb77386ec0179ba73de
Safal Rana <123141574+SafalRana01@users.noreply.github.com>
2023-08-22 21:32:24 +0545

    Update Basic.py

--- Basic.py
+++ Basic.py
@@ -3,3 +3,24 @@
 print('Hello world!')
 
 # ------------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------------------
+
+#  Using of Arthematics Operator
+
+a = int(input("Enter the first number:"))
+b = int(input("Enter the Second number:"))
+
+sum = a + b
+diff = a - b
+mul = a * b
+div = a / b
+rem = a % b
+
+print("The sum of two number is",sum)
+print("The difference of two number is",diff)
+print("The product of two number is",mul)
+print("The division of two number is",div)
+print("The remainder of the number is",rem)
+
+# ------------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------------------
